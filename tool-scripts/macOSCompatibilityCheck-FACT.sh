24777b6a9f3d98aad7aa375c2e6a4e1b9b46cd35
percisely-dd <128342373+percisely-dd@users.noreply.github.com>
2024-06-03 13:14:10 -0700

    Update macOSCompatibilityCheck-FACT.sh

--- tool-scripts/macOSCompatibilityCheck-FACT.sh
+++ tool-scripts/macOSCompatibilityCheck-FACT.sh
@@ -4,7 +4,7 @@
 # Check if the model does support the latest version of macOS using SOFA
 # by Balz Aschwanden
 #
-# adobted from the following script by Graham Pugh
+# adopted from the following script by Graham Pugh
 # https://github.com/macadmins/sofa/blob/main/tool-scripts/macOSVersionCheck-EA.sh
 #
 #################################################
