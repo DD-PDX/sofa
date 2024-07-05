8eb14efb3478c7be0e517fcf93195b5d75b06497
percisely-dd <hulett@davincid.com>
2024-07-05 12:06:54 -0700

    Updated with older macOS support changes from EA

--- tool-scripts/macOSCompatibilityCheck-FACT.sh
+++ tool-scripts/macOSCompatibilityCheck-FACT.sh
@@ -15,16 +15,22 @@
 #  - Updated build check from 21H to 21D		#
 #################################################
 #
-# Note that this uses plutil so is only compatible with macOS 12+
-#
-# Use code inspired by the following post to exit if macOS < 12
-# https://scriptingosx.com/2020/10/dealing-with-xpath-changes-in-big-sur/
-#
-# Modified from original which looked for less than 21H.
-#
-if [[ $(sw_vers -buildVersion) < "21D" ]]; then
-    echo "<result>This EA only supports macOS 12+</result>"
-    exit 1
+# Compatibility for older macOS versions using python added by G Pugh
+
+# Get current system OS
+system_version=$( /usr/bin/sw_vers -productVersion )
+# system_version=14.1  # UNCOMMENT AND ADJUST TO TEST OLDER VERSIONS
+
+system_os=$(cut -d. -f1 <<< "$system_version")
+# echo "System Version: $system_version"
+
+if [[ $system_os -ge 12 ]]; then
+    # use plistlib
+    os_compatibility="current"
+else
+    # use python 2.7
+    os_compatibility="legacy"
+    python_path="/usr/bin/python"
 fi
 
 # URL to the online JSON data
@@ -38,7 +44,7 @@ etag_cache="$json_cache_dir/macos_data_feed_etag.txt"
 # ensure local cache folder exists
 /bin/mkdir -p "$json_cache_dir"
 
-# check local vs online using etag
+# check local vs online using etag (only available on macOS 12+)
 if [[ -f "$etag_cache" && -f "$json_cache" ]]; then
     if /usr/bin/curl --silent --etag-compare "$etag_cache" "$online_json_url" --output /dev/null; then
 #        echo "Cached e-tag matches online e-tag - cached json file is up to date"
@@ -46,6 +52,9 @@ if [[ -f "$etag_cache" && -f "$json_cache" ]]; then
 #         echo "Cached e-tag does not match online e-tag, proceeding to download SOFA json file"
         /usr/bin/curl --location --max-time 3 --silent "$online_json_url" --etag-save "$etag_cache" --output "$json_cache"
     fi
+elif [[ "$os_compatibility" == "legacy" ]]; then
+#     echo "No e-tag cached, proceeding to download SOFA json file"
+    /usr/bin/curl --location --max-time 3 --silent "$online_json_url" --output "$json_cache"
 else
 #    echo "No e-tag cached, proceeding to download SOFA json file"
     /usr/bin/curl --location --max-time 3 --silent "$online_json_url" --etag-save "$etag_cache" --output "$json_cache"
@@ -55,13 +64,18 @@ fi
 
 if [[ ! -f "$json_cache" ]]; then
     echo "<result>Could not obtain data</result>"
-    exit 1
-elif ! plutil -extract "UpdateHash" raw "$json_cache" > /dev/null; then
+    exit
+elif [[ "$os_compatibility" == "legacy" ]]; then
+    if ! "$python_path" -c 'import sys, json; print json.load(sys.stdin)["UpdateHash"]' < "$json_cache" > /dev/null; then
+        echo "<result>Could not obtain data</result>"
+        exit 1
+    fi
+elif ! /usr/bin/plutil -extract "UpdateHash" raw "$json_cache" > /dev/null; then
     echo "<result>Could not obtain data</result>"
     exit 1
 fi
 
-# 1. Get model (DeviceID)
+# Get model (DeviceID)
 model=$(/usr/sbin/sysctl -n hw.model)
 # echo "Model Identifier: $model"
 
@@ -71,18 +85,24 @@ if [[ $model == "VirtualMac"* ]]; then
     model="Macmini9,1"
 elif ! grep -q "$model" "$json_cache"; then
     echo "<result>Unsupported Hardware</result>"
-    exit
+    exit 1
 fi
 
-# 2. identify the latest major OS
-latest_os=$(/usr/bin/plutil -extract "OSVersions.0.OSVersion" raw -expect string "$json_cache" | /usr/bin/head -n 1)
+if [[ "$os_compatibility" == "current" ]]; then
+    # identify the latest major OS (macOS 12+ method)
+    latest_os=$(/usr/bin/plutil -extract "OSVersions.0.OSVersion" raw -expect string "$json_cache" | /usr/bin/head -n 1)
+    # idenfity latest compatible major OS (macOS 12+ method)
+    latest_compatible_os=$(/usr/bin/plutil -extract "Models.$model.SupportedOS.0" raw -expect string "$json_cache" | /usr/bin/head -n 1)
+else
+    # identify the latest major OS (macOS 11- method)
+    latest_os=$("$python_path" -c 'import sys, json; print json.load(sys.stdin)["OSVersions"][0]["OSVersion"]' < "$json_cache" | /usr/bin/head -n 1)
+    # idenfity latest compatible major OS (macOS 11- method)
+    latest_compatible_os=$("$python_path" -c 'import sys, json; print json.load(sys.stdin)["Models"]['"$model"']["SupportedOS"][0]' < "$json_cache" | /usr/bin/head -n 1)
+fi
 # echo "Latest macOS: $latest_os"
-
-# 3. idenfity latest compatible major OS
-latest_compatible_os=$(/usr/bin/plutil -extract "Models.$model.SupportedOS.0" raw -expect string "$json_cache" | /usr/bin/head -n 1)
 # echo "Latest Compatible macOS: $latest_compatible_os"
 
-# 5. Compare latest with compatible
+# Compare latest with compatible
 if [[ "$latest_os" == "$latest_compatible_os" ]];then
     echo "True"
 else
