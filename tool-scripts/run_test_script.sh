76df9ebbf4c5c1e8144142e1437e3313f367385a
percisely-dd <128342373+percisely-dd@users.noreply.github.com>
2024-08-02 18:03:32 -0700

    Create run_test_script.sh

---
+++ tool-scripts/run_test_script.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh --no-rcs
+#
+# Downloads and runs the https://github.com/kirkpatrickprice/macos-auditor script.
+#
+#
+#
+scriptPath="/tmp/test_scripts.sh"
+scriptURL="https://raw.githubusercontent.com/DD-PDX/sofa/main/tool-scripts/test_scripts.sh"
+#
+# Download the script
+/usr/bin/curl -L -s $scriptURL -o $scriptPath
+#
+# Make it executable
+chmod a+x $scriptPath
+#
+# Run the script
+/bin/zsh $scriptPath
