4ebaedf80cec144b515f007524d5ab8cce4c9b7b
percisely-dd <128342373+percisely-dd@users.noreply.github.com>
2024-09-10 10:28:08 -0700

    updated with changes from EAs

--- tool-scripts/macOSCVECheck-FACT.sh
+++ tool-scripts/macOSCVECheck-FACT.sh
@@ -23,29 +23,31 @@ autoload is-at-least
 
 # URL to the online JSON data
 online_json_url="https://sofafeed.macadmins.io/v1/macos_data_feed.json"
-user_agent="SOFA-Jamf-EA-macOSCVECheck/1.0"
+user_agent="SOFA-Jamf-EA-macOSCVECheck/1.1"
 
 # local store
 json_cache_dir="/private/tmp/sofa"
 json_cache="$json_cache_dir/macos_data_feed.json"
 etag_cache="$json_cache_dir/macos_data_feed_etag.txt"
+etag_cache_temp="$json_cache_dir/macos_data_feed_etag_temp.txt"
 
 # ensure local cache folder exists
 /bin/mkdir -p "$json_cache_dir"
 
-# check local vs online using etag
+# check local vs online using etag (only available on macOS 12+)
 if [[ -f "$etag_cache" && -f "$json_cache" ]]; then
-#     echo "e-tag stored, will download only if e-tag doesn't match"
     etag_old=$(/bin/cat "$etag_cache")
-    /usr/bin/curl --compressed --silent --etag-compare "$etag_cache" --etag-save "$etag_cache" --header "User-Agent: $user_agent" "$online_json_url" --output "$json_cache"
-    etag_new=$(/bin/cat "$etag_cache")
-    if [[ "$etag_old" == "$etag_new" ]]; then
+    /usr/bin/curl --compressed --silent --etag-compare "$etag_cache" --etag-save "$etag_cache_temp" --header "User-Agent: $user_agent" "$online_json_url" --output "$json_cache"
+    etag_temp=$(/bin/cat "$etag_cache_temp")
+    if [[ "$etag_old" == "$etag_temp" || $etag_temp == "" ]]; then
 #         echo "Cached ETag matched online ETag - cached json file is up to date"
+        /bin/rm "$etag_cache_temp"
     else
 #         echo "Cached ETag did not match online ETag, so downloaded new SOFA json file"
+        /bin/mv "$etag_cache_temp" "$etag_cache"
     fi
 else
-    echo "No e-tag cached, proceeding to download SOFA json file"
+#     echo "No e-tag cached, proceeding to download SOFA json file"
     /usr/bin/curl --compressed --location --max-time 3 --silent --header "User-Agent: $user_agent" "$online_json_url" --etag-save "$etag_cache" --output "$json_cache"
 fi
 
@@ -96,7 +98,7 @@ for i in {0..3}; do
     if [[ $os_version ]]; then
         if [[ "$os_version" == "$latest_compatible_os" ]]; then
             product_version=$(/usr/bin/plutil -extract "OSVersions.$i.Latest.ProductVersion" raw "$json_cache" | /usr/bin/head -n 1)
-            echo "Latest Compatible macOS Version: $product_version"
+#             echo "Latest Compatible macOS Version: $product_version"
         fi
         if [[ "$os_version" == "$system_os" ]]; then
             os_position=$i
@@ -121,11 +123,11 @@ if ! is-at-least "$product_version" "$system_version"; then
 
         if [[ $product_version ]]; then
             cves_in_version=$(/usr/bin/plutil -extract "OSVersions.$os_position.SecurityReleases.$j.UniqueCVEsCount" raw "$json_cache" 2>/dev/null | grep -v "<stdin>")
-            echo "CVEs in macOS Version $product_version: $cves_in_version"
+#             echo "CVEs in macOS Version $product_version: $cves_in_version"
             cves=$((cves + cves_in_version))
 
             exploits_in_version=$(/usr/bin/plutil -extract "OSVersions.$os_position.SecurityReleases.$j.ActivelyExploitedCVEs" raw "$json_cache" 2>/dev/null | grep -v "<stdin>")
-            echo "Actively exploited CVEs in macOS Version $product_version: $exploits_in_version"
+#             echo "Actively exploited CVEs in macOS Version $product_version: $exploits_in_version"
             exploits=$((exploits + exploits_in_version))
         else
             break
