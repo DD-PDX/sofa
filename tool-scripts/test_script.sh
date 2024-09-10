a63fda7132b254b772ef3952ff08c518811de29b
percisely-dd <128342373+percisely-dd@users.noreply.github.com>
2024-09-10 10:19:05 -0700

    Create test_script.sh

---
+++ tool-scripts/test_script.sh
@@ -0,0 +1,46 @@
+
+#!/bin/zsh
+#
+#
+runScript(){
+# Download the script
+/usr/bin/curl -L -s $scriptURL -o $scriptPath
+#
+# Make it executable
+chmod a+x $scriptPath
+#
+# Run the script
+/bin/zsh $scriptPath
+}
+#
+cd /tmp/
+rm -rf /tmp/*FACT.sh
+echo "Exploit Check:"
+scriptPath="/tmp/macOSActiveExploitCheck-NUMERIC-FACT.sh"
+scriptURL="https://raw.githubusercontent.com/DD-PDX/sofa/main/tool-scripts/macOSActiveExploitCheck-NUMERIC-FACT.sh"
+runScript
+echo "CVE Check:"
+scriptPath="/tmp/macOSCVECheck-NUMERIC-FACT.sh"
+scriptURL="https://raw.githubusercontent.com/DD-PDX/sofa/main/tool-scripts/macOSCVECheck-NUMERIC-FACT.sh"
+runScript
+echo "Compatibility Check:"
+scriptPath="/tmp/macOSCompatibilityCheck-FACT.sh"
+scriptURL="https://raw.githubusercontent.com/DD-PDX/sofa/main/tool-scripts/macOSCompatibilityCheck-FACT.sh"
+runScript
+echo "Version Check:"
+scriptPath="/tmp/macOSVersionCheck-FACT.sh"
+scriptURL="https://raw.githubusercontent.com/DD-PDX/sofa/main/tool-scripts/macOSVersionCheck-FACT.sh"
+runScript
+echo "XProtect Version Check:"
+scriptPath="/tmp/XProtectVersionCheck-FACT.sh"
+scriptURL="https://raw.githubusercontent.com/DD-PDX/sofa/main/tool-scripts/XProtectVersionCheck-FACT.sh"
+runScript
+echo "macOS Displacement Check:"
+scriptPath="/tmp/macOSVersionDisplacement-FACT.sh"
+scriptURL="https://raw.githubusercontent.com/DD-PDX/sofa/main/tool-scripts/macOSVersionDisplacement-FACT.sh"
+runScript
+
+echo "Verbose CVE Check:"
+scriptPath="/tmp/macOSCVECheck-FACT.sh"
+scriptURL="https://raw.githubusercontent.com/DD-PDX/sofa/main/tool-scripts/macOSCVECheck-FACT.sh"
+runScript
